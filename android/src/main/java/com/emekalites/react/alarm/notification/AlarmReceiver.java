aa9aaf798513fd3df1f8f63fbf875e38e7918b37
Hussain <hussainofficial53@gmail.com>
2020-10-13 23:24:13 +0530

    Initial commit

--- android/src/main/java/com/emekalites/react/alarm/notification/AlarmReceiver.java
+++ android/src/main/java/com/emekalites/react/alarm/notification/AlarmReceiver.java
@@ -19,6 +19,7 @@ public class AlarmReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.e("MUMINO","ALARM RECEIEVED");
         if (intent != null) {
             final AlarmDatabase alarmDB = new AlarmDatabase(context);
             AlarmUtil alarmUtil = new AlarmUtil((Application) context.getApplicationContext());
